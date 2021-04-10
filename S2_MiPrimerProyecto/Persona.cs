764da93cf64be61368ca5f9e6acfa17f698f4624
Wendy Mendoza Pérez <wendy_25_06@outlook.com>
2021-04-10 09:20:47 -0500

    proyecto

---
+++ S2_MiPrimerProyecto/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2_MiPrimerProyecto
+{
+    public class Persona
+    {
+        public string dni { get; set; }
+        public string nombreCompleto { get; set; }
+        public int telefono { get; set; }
+        
+        public Persona()
+        {
+
+        }
+
+        public Persona(string dni, string nombreCompleto, int telefono) 
+        {
+            this.dni = dni;
+            this.nombreCompleto = nombreCompleto;
+            this.telefono = telefono;     
+        
+        }
+               
+         
+
+    }
+}
---
+++ S2_MiPrimerProyecto/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace S2_MiPrimerProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombreCurso = "Desarrollo de Aplicaciones Web 2021-1";
+            int edad = 26;
+            double monto = 350.50;
+            string salario = "Mil dolares";
+            var nuevaedad = "25";
+
+            string detalleEdad = String.Empty;
+
+            List<Persona> listado = new List<Persona>();
+            Persona per1 = new Persona();
+            per1.dni = "12345678";
+            per1.nombreCompleto = "Jefferson Farfan";
+            per1.telefono = 9858877;
+                      
+            Persona per2 = new Persona("35235235", "Paolo Guerrero", 23523654);
+
+            listado.Add(per1);
+            listado.Add(per2);
+            listado.Add(new Persona("4354366", "Raul Ruidiaz", 43654757));
+
+            detalleEdad = edad > 17 ? "Es mayor de edad" : "Es menor de edad";
+            //if (edad > 17)
+            //{
+            //    detalleEdad = "Es mayor de edad";
+            //}
+            //else
+            //{
+            //    detalleEdad = "Es menor de edad";
+            //}
+
+            foreach (var item in listado)
+            {
+                Console.WriteLine(item.dni + " " + item.nombreCompleto + " " + item.telefono);
+            }
+
+            var busqueda = listado.Where(x => x.dni == "4354366").FirstOrDefault();
+            Console.WriteLine("La busqueda encontró a: " + busqueda.nombreCompleto);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
